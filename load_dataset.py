10890177fd5f9adbe2b236ebfabe89c79ee8fdb9
Neha Jalan <neha@Nehas-MacBook-Pro.local>
2019-02-03 20:37:53 -0800

    fixing bug

--- load_dataset.py
+++ load_dataset.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import train_test_split
 def get_training_data(img_set_size, partition_ratio, img_size):
     # Preparing the training data
     data_dir_list = ['vehicles_test/', 'vehicles_train/']
-    data_dir_path = '/Users/tanmesh/dev/traffic'
+    data_dir_path = '/Users/neha/Downloads/'
     img_data_list = []
     labels = []
     for dataset in data_dir_list:
@@ -49,9 +49,21 @@ def get_training_data(img_set_size, partition_ratio, img_size):
 
     return x_train, x_test, y_train, y_test, classes
 
+def test_back_prop_model(model, image_path, img_size):
+    img = image.load_img(image_path, target_size=(img_size, img_size))
+    img = image.img_to_array(img)
+    plt.imshow(img / 255.)
+    plt.show()
+    x = preprocess_input(np.expand_dims(img.copy(), axis=0))
+    print("test image shape : " + str(x.shape))
+    height = x.shape[1]
+    width = x.shape[2]
+    n_channels = x.shape[3]
+    x = x.reshape((1, height * width * n_channels))
+    print(model.predict(x))
 
-def test_model(model, image_path):
-    img = image.load_img(image_path, target_size=(224, 224))
+def test_model(model, image_path, img_size = 224):
+    img = image.load_img(image_path, target_size=(img_size, img_size))
     img = image.img_to_array(img)
     plt.imshow(img / 255.)
     plt.show()
--- using_back_prop.py
+++ using_back_prop.py
@@ -3,7 +3,7 @@ import time
 from tensorflow.python.keras import Sequential, optimizers
 from tensorflow.python.layers.core import Dense
 import matplotlib.pyplot as plt
-from load_dataset import get_training_data, test_model
+from load_dataset import get_training_data, test_back_prop_model
 import numpy as np
 
 
@@ -19,61 +19,37 @@ def evaluate_model(history):
     plt.show()
 
 
-def build_model():
+def build_model(h,w,n_c):
     model = Sequential()
-    model.add(Dense(32, input_dim=(16,), activation="relu"))
-    model.add(Dense(16, activation="relu"))
+    model.add(Dense(128, input_dim=h*w*n_c, activation="relu"))
+    model.add(Dense(128, activation="relu"))
     model.add(Dense(1, activation="sigmoid"))
     return model
 
 
 def using_back_prop(img_set_size):
-    x_train, x_test, y_train, y_test, classes = get_training_data(img_set_size, partition_ratio=0.2, img_size=64)
+    img_size = 64
+    x_train, x_test, y_train, y_test, classes = get_training_data(img_set_size, partition_ratio=0.2, img_size=img_size)
 
-    print(type(x_train))
-    print(type(x_test))
-    print(type(y_train))
-    print(type(y_test))
+    n_train = x_train.shape[0]
+    n_test = x_test.shape[0]
+    height = x_train.shape[1]
+    width = x_train.shape[2]
+    n_channels = x_train.shape[3]
+    print(n_train, n_test, height, width, n_channels)
 
-    # img = image.img_to_array(x_train[0])
-    # plt.imshow(img / 255.)
-    # plt.show()
-    #
-    # img = image.img_to_array(x_test[0])
-    # plt.imshow(img / 255.)
-    # plt.show()
-    #
-    # print(y_train[0])
-    # print(y_test[0])
-
-    m_train = x_train.shape[0]
-    num_px = x_train.shape[1]
-    m_test = x_test.shape[0]
-
-    print("Number of training examples: " + str(m_train))
-    print("Number of testing examples: " + str(m_test))
-    print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
-    print("train_x_orig shape: " + str(x_train.shape))
-    print("train_y shape: " + str(y_train))
-    print("test_x_orig shape: " + str(x_test.shape))
-    print("test_y shape: " + str(y_test))
-
-    x_train_flatten = x_train.reshape(x_train.shape[0], -1).T
-    x_test_flatten = x_test.reshape(x_test.shape[0], -1).T
-
-    x_train = x_train_flatten/255.
-    x_test = x_test_flatten / 255.
-
+    x_train = x_train.reshape((n_train, height * width * n_channels))
+    x_test = x_test.reshape((n_test, height * width * n_channels))
     print("train_x's shape: " + str(x_train.shape))
     print("test_x's shape: " + str(x_test.shape))
 
-    # n_x = num_px * num_px * 3
-    # n_h = 7
-    # n_y = 1
+    # x_train = x_train_flatten/255.
+    # x_test = x_test_flatten / 255.
 
-    model = build_model()
+    model = build_model(height,width,n_channels)
 
     model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=2e-5), metrics=['accuracy'])
+    t = time.time()
     hist = model.fit(x_train, y_train, batch_size=32, epochs=2, validation_data=(x_test, y_test))
     print('Training time: %s' % (t - time.time()))
     (loss, accuracy) = model.evaluate(x_test, y_test, batch_size=32, verbose=1)
@@ -82,14 +58,14 @@ def using_back_prop(img_set_size):
     evaluate_model(hist)
 
     # negative scenario
-    test_image_path = '/Users/tanmesh/dev/traffic/vehicles_test/4593_not_vehicle.jpg'
-    test_model(model, test_image_path)
+    test_image_path = '/Users/neha/Downloads/vehicles_test/4593_not_vehicle.jpg'
+    test_back_prop_model(model, test_image_path, img_size)
 
     # positive scenario
-    test_image_path = '/Users/tanmesh/dev/traffic/vehicles_train/02033_vehicle.jpg'
-    test_model(model, test_image_path)
+    test_image_path = '/Users/neha/Downloads/vehicles_train/02033_vehicle.jpg'
+    test_back_prop_model(model, test_image_path, img_size)
 
 
 # fix random seed for reproducibility
 np.random.seed(7)
-using_back_prop(img_set_size=50)
+using_back_prop(img_set_size=20000)
